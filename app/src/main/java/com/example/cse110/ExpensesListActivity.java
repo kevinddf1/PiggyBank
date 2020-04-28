a2bed884183f6a7b7355a6e87596eb662221f07b
Peter Gonzalez <p8gonzal@ucsd.edu>
2020-04-27 23:16:47 -0700

    added fine tuning for budget and expense limits

--- app/src/main/java/com/example/cse110/ExpensesListActivity.java
+++ app/src/main/java/com/example/cse110/ExpensesListActivity.java
@@ -18,13 +18,15 @@ import java.util.Date;
 public class ExpensesListActivity extends AppCompatActivity {
     public static final String MONTHLY_DATA_INTENT = "ExpenseListActivity monthlyData";
     public static final String CATEGORY_NAME_INTENT = "ExpenseListActivity categoryName";
-
+    private static  final int MAX_EXPENSE_VALUE = 9999999;
     private EditText expenseName, expenseCost;
     //List Structure
     private ExpenseListAdapter expenseAdapter;
 
     private MonthlyData monthlyData;
     private Category category;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,11 +69,21 @@ public class ExpensesListActivity extends AppCompatActivity {
                 // Ensure that both fields are filled.
                 if(!expenseCost.getText().toString().isEmpty() && !expenseName.getText().toString().isEmpty() ) {
 
-                    // Create new item and update adapter
-                    category.createExpense(expenseName.getText().toString(), Double.parseDouble(expenseCost.getText().toString()), today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH));
-                    expenseName.getText().clear();
-                    expenseCost.getText().clear();
-                    expenseAdapter.notifyDataSetChanged();
+                    //Check that we do not go over the max allowed expense
+                    try {
+                        if(Double.parseDouble(expenseCost.getText().toString()) > MAX_EXPENSE_VALUE)
+                            throw new Exception();
+
+                        // Create new item and update adapter
+                        category.createExpense(expenseName.getText().toString(), Double.parseDouble(expenseCost.getText().toString()), today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH));
+                        expenseName.getText().clear();
+                        expenseCost.getText().clear();
+                        expenseAdapter.notifyDataSetChanged();
+                    }catch (Exception overflow){
+                            Toast.makeText(getBaseContext(), "The max expense value is $9,999,999", Toast.LENGTH_LONG).show();
+                    }
+
+
                 } else {
                     // Insufficient number of filled fields results in an error warning.
                     Toast missingInformationWarning = Toast.makeText(getBaseContext(), "Missing Information", Toast.LENGTH_SHORT);
