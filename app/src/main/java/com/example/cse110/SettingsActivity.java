db2e2a007c3c5aacb27006c170c40997b53f4b12
Miv99 <treecam42@gmail.com>
2020-05-04 18:50:29 -0700

    Add sign out and account deletion functionality

--- app/src/main/java/com/example/cse110/SettingsActivity.java
+++ app/src/main/java/com/example/cse110/SettingsActivity.java
@@ -1,10 +1,20 @@
 package com.example.cse110;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -26,6 +36,42 @@ public class SettingsActivity extends AppCompatActivity {
         notificationsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 settings.setEnableNotifications(isChecked);
+            }
+        });
+
+        final Button signOutButton = findViewById(R.id.sign_out_button);
+        signOutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.signOut();
+
+                // Start login activity
+                Intent i = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        final Button deleteAccountButton = findViewById(R.id.delete_account_button);
+        deleteAccountButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.getCurrentUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getBaseContext(), "Account deletion failed. " + task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+                // Start login activity
+                Intent i = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(i);
             }
         });
     }
