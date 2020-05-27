33182f690dcf53b6c8bf502ceae9faaa3ecf8c91
erdong021 <49662078+erdong021@users.noreply.github.com>
2020-05-27 16:23:36 -0700

    current version for team

--- app/src/main/java/com/example/cse110/View/MainActivity.java
+++ app/src/main/java/com/example/cse110/View/MainActivity.java
@@ -261,6 +261,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
