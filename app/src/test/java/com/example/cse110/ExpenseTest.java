b8c828c42a890aba8e12d99f1d6e6678f5985bd9
Brenton Porter <bcporter@ucsd.edu>
2020-05-28 11:58:37 -0700

    Working on Unit Tests for Controller files

--- app/src/test/java/com/example/cse110/ExpenseTest.java
+++ app/src/test/java/com/example/cse110/ExpenseTest.java
@@ -2,13 +2,26 @@ package com.example.cse110;
 import com.example.cse110.Controller.Expense;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ExpenseTest {
+
+    Expense formatOneDecimal;
+    Expense formatOneDecOnePlace;
+    Expense formatCorrect;
+
+    @Before
+    public void setup() {
+        formatOneDecimal = new Expense(0, "test", 1., 5, 5, 5, null);
+        formatOneDecOnePlace = new Expense(0, "test", 1.5, 5, 5, 5, null);
+        formatCorrect = new Expense(5, "Cheeseburger", 35.63, 2, 6, 26, null);
+    }
+
     @Test
     public void constructor_null_and_invalid_inputs() {
         try {
-            Expense test = new Expense(Integer.MAX_VALUE, null, 0, 13, 100, 100, null);
+            Expense willFail = new Expense(Integer.MAX_VALUE, null, 0, 13, 100, 100, null);
             Assert.fail();
         } catch (Exception e) {
             Assert.assertTrue(true);
@@ -17,13 +30,16 @@ public class ExpenseTest {
 
     @Test
     public void constructor_double_with_only_period() {
-        Expense test = new Expense(0, "test", 1., 5, 5, 5, null);
-        Assert.assertEquals("1.00", test.getCostAsString());
+        Assert.assertEquals("1.00", formatOneDecimal.getCostAsString());
     }
 
     @Test
     public void constructor_double_with_only_1_num() {
-        Expense test = new Expense(0, "test", 1.5, 5, 5, 5, null);
-        Assert.assertEquals("1.50", test.getCostAsString());
+        Assert.assertEquals("1.50", formatOneDecOnePlace.getCostAsString());
+    }
+
+    @Test
+    public void constructor_correct_input() {
+        Assert.assertEquals("35.63", formatCorrect.getCostAsString());
     }
 }
