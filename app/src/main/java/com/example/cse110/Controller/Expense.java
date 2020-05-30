78ad93a0bd57a793e4be6b412a1822e8d8d6645f
Peter Gonzalez <p8gonzal@ucsd.edu>
2020-05-30 16:48:03 -0700

    Finished styling for MainActivity and Expense

--- app/src/main/java/com/example/cse110/Controller/Expense.java
+++ app/src/main/java/com/example/cse110/Controller/Expense.java
@@ -3,26 +3,41 @@ package com.example.cse110.Controller;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.example.cse110.Model.FormattingTool;
+
 /**
- * An Expense should be instantiated only by Category.
+ * An Expense object that stores information for an expense logged by the user.
+ * Author: Peter Gonzalez
  */
 public class Expense implements Parcelable {
+
+
+    public static final int DECEMBER = 11;
+    /**
+     * Private variables for attrivbutes of an expense object;
+     */
     private String name;
     private int cost;
     private int year, month, day;
     private int id;
+
+
     // parentCategoryName should not be saved in the database
     private String parentCategoryName;
     private String costString;
 
+    //Formatting tool to avoid redundancy
+    private FormattingTool formattingTool = new FormattingTool();
+
     /**
      * Constructor for the Expense object
-     * @param id - unique identification for the Expense object
-     * @param name - name of the expense
-     * @param cost - cost of the expense
-     * @param year - year the expense was logged
-     * @param month - month the expense was logged
-     * @param day - day the expense was logged
+     *
+     * @param id                 - unique identification for the Expense object
+     * @param name               - name of the expense
+     * @param cost               - cost of the expense
+     * @param year               - year the expense was logged
+     * @param month              - month the expense was logged
+     * @param day                - day the expense was logged
      * @param parentCategoryName - the category that the expense belongs to
      * @return An expense object
      */
@@ -30,34 +45,15 @@ public class Expense implements Parcelable {
         this.id = id;
         this.name = name;
         costString = Double.toString(cost);
+
         // Throws a NullPointerException if the input names are null or the month is invalid
-        if (name.equals(null) || costString.equals(null) || month <= 0 || month > 12) {
+        if (name.equals(null) || costString.equals(null) || month <= 0 || month > DECEMBER) {
             throw new NullPointerException("Name and cost must not be null. Month must " +
                     "be greater than or equal to 0.");
         }
-        // Add formatting for whole numbers
-        if(costString.indexOf('.') == -1){
-            costString = costString.concat(".00");
-        }else{
-            //Ensure only valid input
-            int costLength = costString.length();
-            int decimalPlace = costString.indexOf(".");
 
-            // If the user inputs a number formatted as "<num>.", appends a 00 after the decimal
-            if (costLength - decimalPlace == 1) {
-                costString = costString.substring(0, decimalPlace + 1) +  "00";
-            }
-            // If the user inputs a number formatted as "<num>.1", where 1 could be any number,
-            // appends a 0 to the end
-            else if (costLength - decimalPlace == 2) {
-                costString = costString.substring(0, decimalPlace + 1 + 1) + "0";
-            }
-            // If the user inputs a number with >= 2 decimal places, only displays up to 2
-            else {
-                costString = costString.substring(0, costString.indexOf(".") + 2 + 1);
-            }
-        }
-        this.costString = costString;
+
+        this.costString = formattingTool.formatDecimal(costString);
         this.cost = (int) (100 * Double.parseDouble(costString));
         this.month = month;
         this.year = year;
@@ -68,6 +64,7 @@ public class Expense implements Parcelable {
     /**
      * Constructor for an Expense object that reads in serialized Expense data
      * and converts it into a usable object
+     *
      * @param in - parcel data to convert
      */
     protected Expense(Parcel in) {
@@ -81,6 +78,10 @@ public class Expense implements Parcelable {
         costString = in.readString();
     }
 
+
+    /**
+     * Read information in from the parcel
+     */
     public static final Creator<Expense> CREATOR = new Creator<Expense>() {
         @Override
         public Expense createFromParcel(Parcel in) {
@@ -93,73 +94,120 @@ public class Expense implements Parcelable {
         }
     };
 
-//    /**
-//     * Update the database to reflect changes in Expense's fields.
-//     * This is called every time any field in Expense is modified.
-//     */
-//    public void updateToDatabase() {
-//        // String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-//        // TODO: uid uniquely identifies the user; use it to update the database; Expense.id uniquely identifies the Expense within the Category
-//        // use uid, id, and parentCategoryName
-//    }
 
+    /**
+     * Getter for name of the category
+     *
+     * @return
+     */
     public String getName() {
         return name;
     }
 
     /**
-     * As cents.
-     * @return
+     * The cost of the expense, represented as cents.
+     *
+     * @return The int represent the original expense cost * 100
      */
     public int getCost() {
         return cost;
     }
+
+    /**
+     * Getter for the year the expense was logged.
+     *
+     * @return The year as an int
+     */
     public int getYear() {
         return year;
     }
+
+    /**
+     * Getter for the month the expense was logged.
+     *
+     * @return The month as an int, starting at index 0
+     */
     public int getMonth() {
         return month;
     }
+
+    /**
+     * The getter for the day the expense was logged
+     *
+     * @return The day as an int, starting at index 1
+     */
     public int getDay() {
         return day;
     }
+
+    /**
+     * Unique id to this expense
+     *
+     * @return The unique to id to the expense, help's deletion.
+     */
     public int getId() {
         return id;
     }
+
+    /**
+     * Return the formatted cost of the expense.
+     *
+     * @return A string with corrct decimal placement
+     */
     public String getCostAsString() {
-        // TODO
         return costString;
     }
 
+    /**
+     * Set a unique id for the expense.
+     *
+     * @param id The id to assign to the expense.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * The name of the expense, not unique.
+     *
+     * @param name The name to assign to the expense
+     */
     public void setName(String name) {
         this.name = name;
-        //updateToDatabase();
-        //base.insertExpenseName(name);
     }
+
+    /**
+     * Setter to assign a cost value to the expense.
+     *
+     * @param cost The cost of the expense to assign.
+     */
     public void setCost(int cost) {
         this.cost = cost;
-        //updateToDatabase();
-        //base.insertExpenseCost(cost);
+
     }
-    public void setDate(int year, int month, int day) {
-        this.year = year;
-        this.month = month;
-        this.day = day;
-        //updateToDatabase();
-    }
+
+    /**
+     * Reassigns to which this expense belongs to.
+     *
+     * @param parentCategoryName The category to which this expense belongs to.
+     */
     public void setParentCategoryName(String parentCategoryName) {
         this.parentCategoryName = parentCategoryName;
     }
 
+    /**
+     * Neccesary for all parcelable classes
+     *
+     * @return
+     */
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+     * Write to the parcel, the information to serialize.
+     */
     @Override
     public void writeToParcel(Parcel parcel, int i) {
         parcel.writeString(name);
--- app/src/main/java/com/example/cse110/View/MainActivity.java
+++ app/src/main/java/com/example/cse110/View/MainActivity.java
@@ -12,41 +12,65 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cse110.Controller.MonthlyData;
+import com.example.cse110.Model.Database;
 import com.example.cse110.Model.FormattingTool;
 import com.example.cse110.R;
-
-import java.util.Calendar;
-
 import com.example.cse110.View.graphs.GraphsActivity;
 import com.example.cse110.View.history.HistoryActivity;
-
-import com.example.cse110.Model.Database;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Calendar;
+
+/**
+ * The  main menu page that allows the user to add expense or track spending.
+ * @author The Technical Team
+ */
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout expenseListButton, historyButton, GraphsButton, settingsButton;
-    public static final String MONTHLY_DATA_INTENT = "CategoriesListActivity monthlyData";
-    public static final String HISTORY_DATA_INTENT = "HistoryActivity monthlyData";
-    public static final String Graphs_DATA_INTENT = "GraphsActivity monthlyData";
+    /**
+     * Icon setup
+     */
+    private LinearLayout expenseListButton;
+    private LinearLayout historyButton;
+    private LinearLayout GraphsButton;
+    private LinearLayout settingsButton;
+
+    /**
+     * Keys for pulling/pushing data between pages
+     */
+    private static final String MONTHLY_DATA_INTENT = "CategoriesListActivity monthlyData";
+    private static final String HISTORY_DATA_INTENT = "HistoryActivity monthlyData";
+    private static final String Graphs_DATA_INTENT = "GraphsActivity monthlyData";
     private static final String LIST_OF_MONTHS = "List of Months"; //For past months in HistoryActivity.java
 
+    /**
+     * Core store element for the user's data
+     */
     private MonthlyData thisMonthsData;
 
-    private Database base = Database.Database(); // create a Database object
+    /**
+     * Allows us to pull most up to date info from the database.
+     */
+    private final Database base = Database.Database(); // create a Database object
 
     /**
      * Formatting tool to avoid redundancies.
      */
-    private FormattingTool formattingTool = new FormattingTool();
+    private final FormattingTool formattingTool = new FormattingTool();
+
     /**
      * TextViews to display budget and total expenses
      */
-    TextView totalBudgetDisplay, totalExpenseDisplay;
+    private TextView totalBudgetDisplay;
+    private TextView totalExpenseDisplay;
 
+    /**
+     * Display all user's total budget and expenses, as well as gives them options.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,9 +83,40 @@ public class MainActivity extends AppCompatActivity {
         //set up nav bar
         setUpNavBar();
 
-        //Instantiate monthlyData only if currently null
-       // if(thisMonthsData == null){
-         //   base.
+
+        //If we are called internally, display local data
+        if (thisMonthsData != null) {
+
+            //Render total budget
+            totalBudgetDisplay = findViewById(R.id.currentCash);
+            String budgetRendering = "Total Budget: $" + formattingTool.formatIntMoneyString(Long.toString(thisMonthsData.getTotalBudget()));
+            totalBudgetDisplay.setText(budgetRendering);
+
+            //Render total expenses
+            totalExpenseDisplay = findViewById(R.id.totalExpenses);
+            String expenseRendering = "Total Expenses: $" + formattingTool.formatMoneyString(formattingTool.formatDecimal(Long.toString(thisMonthsData.getTotalExpensesAsCents() / 100)));
+            totalExpenseDisplay.setText(expenseRendering);
+        }
+        //Upon first login, use the bundle set up by the database
+        else {
+
+            // Get Bundle object that contain the array
+            Bundle b = this.getIntent().getExtras();
+            assert b != null;
+            String[] list = b.getStringArray("Total Budget and Expense");
+
+            //Render total budget
+            totalBudgetDisplay = findViewById(R.id.currentCash);
+            assert list != null;
+            String budgetRendering = "Total Budget: $" + formattingTool.formatIntMoneyString(list[0]);
+            totalBudgetDisplay.setText(budgetRendering);
+
+            //Render total expenses
+            totalExpenseDisplay = findViewById(R.id.totalExpenses);
+            String expensesRendering = "Total Expenses: $" + formattingTool.formatMoneyString(formattingTool.formatDecimal(Double.toString(Long.parseLong(list[1]) / 100.00)));
+            totalExpenseDisplay.setText(expensesRendering);
+        }
+
         //Bind button to go to expense list
         expenseListButton = findViewById(R.id.ExpensesButton);
         expenseListButton.setOnClickListener(new View.OnClickListener() {
@@ -72,90 +127,93 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if(thisMonthsData != null) {
-            totalBudgetDisplay = findViewById(R.id.currentCash);
-            String budgetRendering = "Total Budget: $" + formattingTool.formatIntMoneyString(Long.toString(thisMonthsData.getTotalBudget()));
-            totalBudgetDisplay.setText(budgetRendering);
-
-            totalExpenseDisplay = findViewById(R.id.totalExpenses);
-            String expenseRendering = "Total Expenses: $" + formattingTool.formatMoneyString(formattingTool.formatDecimal(Long.toString(thisMonthsData.getTotalExpensesAsCents()/100)));
-
-            totalExpenseDisplay.setText(expenseRendering);
-        } else {
-            // Get Bundle object that contain the array
-            Bundle b = this.getIntent().getExtras();
-            String[] list = b.getStringArray("Total Budget and Expense");
-
-            //Bind our month's expenses and budget to proper display
-            totalBudgetDisplay = findViewById(R.id.currentCash);
-            String budgetRendering = "Total Budget: $" + formattingTool.formatIntMoneyString( list[0]);
-            totalBudgetDisplay.setText(budgetRendering);
-
-            totalExpenseDisplay = findViewById(R.id.totalExpenses);
-            String expensesRendering = "Total Expenses: $" + formattingTool.formatMoneyString(formattingTool.formatDecimal(Double.toString(Long.parseLong(list[1])/100.00)));
-            totalExpenseDisplay.setText(expensesRendering);
-        }
-
+        //Set up history icon
         historyButton = findViewById(R.id.HistoryButton);
-        historyButton.setOnClickListener(new View.OnClickListener(){
+        historyButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 onHistoryClick(v);
             }
         });
 
+        //Set up graphs icon
         GraphsButton = findViewById(R.id.GraphsButton);
-        GraphsButton.setOnClickListener(new View.OnClickListener(){
+        GraphsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 onGraphsClick(v);
             }
         });
 
 
+        //Set up settings icon
         settingsButton = findViewById(R.id.SettingsButton);
-        settingsButton.setOnClickListener(new View.OnClickListener(){
+        settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 onSettingsClick(v);
             }
         });
     }
 
-    // TODO: Month Year UPDATE FROM CATEGORY
+    /**
+     * When the expenses icon is clicked, the request is directed here.
+     * @param v
+     */
     public void onExpensesCLick(View v) {
         listsPageHandler();
     }
 
-    private void onHistoryClick(View v){
+    /**
+     * When the history icon is clicked, the user is directed here.
+     * @param v
+     */
+    private void onHistoryClick(View v) {
         historyPageHandler();
     }
 
-    private void onGraphsClick(View v){
+    /**
+     * When the graph icon is clicked, the user is directed here.
+     * @param v
+     */
+    private void onGraphsClick(View v) {
         graphPageHandler();
     }
 
 
-    public void onSettingsClick(View v) {
+    /**
+     * Settings clock goes straight to settings page
+     * @param v
+     */
+    private void onSettingsClick(View v) {
         Intent intent = new Intent(getBaseContext(), SettingsActivity.class);
         startActivity(intent);
     }
 
 
+    /**
+     * Executes upon backpress from CategoryListActivity.
+     * @param requestCode
+     * @param resultCode
+     * @param data The intent containing the most recent monthly data
+     */
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1) {
             if (resultCode == RESULT_OK) {
+
+                //Get most recent data
                 thisMonthsData = data.getParcelableExtra(CategoriesListActivity.MONTHLY_DATA_INTENT);
 
+                //Render total budget
                 totalBudgetDisplay = findViewById(R.id.currentCash);
                 String budgetRendering = "Total Budget: $" + formattingTool.formatIntMoneyString(Long.toString(thisMonthsData.getTotalBudget()));
                 totalBudgetDisplay.setText(budgetRendering);
 
+                //Render total expenses
                 totalExpenseDisplay = findViewById(R.id.totalExpenses);
-                String expenseRendering = "Total Expenses: $" + formattingTool.formatMoneyString(formattingTool.formatDecimal(Double.toString(thisMonthsData.getTotalExpensesAsCents()/100.00)));
-
+                String expenseRendering = "Total Expenses: $" + formattingTool.formatMoneyString(formattingTool.formatDecimal(Double.toString(thisMonthsData.getTotalExpensesAsCents() / 100.00)));
                 totalExpenseDisplay.setText(expenseRendering);
             }
         }
@@ -167,8 +225,10 @@ public class MainActivity extends AppCompatActivity {
     private void setUpNavBar() {
         // Create the bottom navigation bar
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // make all icons' names visible
         navView.setLabelVisibilityMode(1);
+
         // Check the icon
         Menu menu = navView.getMenu();
         MenuItem menuItem = menu.getItem(0);
@@ -177,10 +237,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Helper method to contain the logic for navigation bar to navigate to the lists page
+     * Helper method to contain the logic for navigation bar to navigate to the lists (expenses) page.
      */
     private void listsPageHandler() {
-        /* Read from the database
+        /** Read from the database
         / Read data once: addListenerForSingleValueEvent() method triggers once and then does not trigger again.
         / This is useful for data that only needs to be loaded once and isn't expected to change frequently or require active listening.
         */
@@ -189,19 +249,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Intent intent = new Intent(getBaseContext(), CategoriesListActivity.class);
+
                 //set up the date for monthly data
                 Calendar today = Calendar.getInstance();
                 int month = today.get(Calendar.MONTH);
                 int year = today.get(Calendar.YEAR);
                 base.insertMonthlydata(year, month);
+
                 //Retrieve the monthly data from the database
                 thisMonthsData = base.RetrieveDataCurrent(dataSnapshot, thisMonthsData, year, month);
+
                 //put extra data into new intent
                 intent.putExtra(CategoriesListActivity.MONTHLY_DATA_INTENT, thisMonthsData);
                 startActivityForResult(intent, 1);
+
                 //avoid shifting
                 overridePendingTransition(0, 0);
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // Failed to read value
@@ -215,24 +280,32 @@ public class MainActivity extends AppCompatActivity {
      */
     private void historyPageHandler() {
         ValueEventListener Listener = new ValueEventListener() {
+
             //The onDataChange() method is called every time data is changed at the specified database reference, including changes to children.
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Intent i = new Intent(getBaseContext(), HistoryActivity.class);
+
                 //set up the date for monthly data
                 Calendar today = Calendar.getInstance();
                 int month = today.get(Calendar.MONTH);
                 int year = today.get(Calendar.YEAR);
+
                 //Retrieve the monthly data from the database
                 thisMonthsData = base.RetrieveDataCurrent(dataSnapshot, thisMonthsData, year, month);
+
                 //put extra data into new intent
                 i.putExtra(HISTORY_DATA_INTENT, thisMonthsData);
-                //Add the past month's history (includes current)e
+
+                //Add the past month's history (includes current)
                 i.putExtra(LIST_OF_MONTHS, base.getPastMonthSummary(dataSnapshot));
                 startActivityForResult(i, 1);
+
+
                 //avoid shifting
                 overridePendingTransition(0, 0);
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // Failed to read value
@@ -242,7 +315,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Helper method to contain the logic for navigation bar to navigate to the graph page
+     * Helper method to contain the logic for navigation bar to navigate to the graph page.
      */
     private void graphPageHandler() {
         base.getMyRef().addListenerForSingleValueEvent(new ValueEventListener() {
@@ -250,15 +323,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Intent i = new Intent(getBaseContext(), GraphsActivity.class);
+
                 //set up the date for monthly data
                 Calendar today = Calendar.getInstance();
                 int month = today.get(Calendar.MONTH);
                 int year = today.get(Calendar.YEAR);
-                //Retrieve the monthly data from the database
 
+                //Retrieve the monthly data from the database
                 thisMonthsData = base.RetrieveDataCurrent(dataSnapshot, thisMonthsData, year, month);
+
                 //Add the past month's history (includes current)e
                 i.putExtra(Graphs_DATA_INTENT, thisMonthsData);
+
                 //Add the past month's history (includes current)e
                 i.putExtra(LIST_OF_MONTHS, base.getPastMonthSummary(dataSnapshot));
                 startActivity(i);
@@ -266,6 +342,7 @@ public class MainActivity extends AppCompatActivity {
                 //avoid shifting
                 overridePendingTransition(0, 0);
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // Failed to read value
@@ -274,9 +351,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * method which controls the nav bar
+     * Method controls the navBar and directs user accordingly.
      */
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -303,9 +380,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
+    /**
+     * Do nothing on back button press because we don't want the user to be able to go back to login page
+     */
     @Override
     public void onBackPressed() {
-        // Do nothing on back button press because we don't want the user to be able to go back to login page
     }
 }
 
